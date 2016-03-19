1554527a7ee0d8974f78630fd6bcc7970568226d
Petr Mánek <petr.manek@seznam.cz>
2016-03-19 17:32:02 +0100

    Fixed randomizable for discrete types.

--- Revolver/Revolver/Discrete+Randomizable.swift
+++ Revolver/Revolver/Discrete+Randomizable.swift
@@ -1,10 +1,10 @@
 extension Discrete {
     // All discrete types are also randomizable.
-    public static func random(generator: EntropyGenerator) -> Self {
+    public init(generator: EntropyGenerator) {
         let count = Self.allValues.count
         precondition(count > 0, "There has to be at least one value in the allValues array.")
         
         let random = generator.nextInRange(min: 0, max: count - 1)
-        return Self.allValues[random]
+        self = Self.allValues[random]
     }
 }
