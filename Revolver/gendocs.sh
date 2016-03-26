d5547edb89f49140a04006d2d11097f08c4d4ba0
Petr Mánek <petr.manek@seznam.cz>
2016-03-27 00:07:26 +0100

    Updated gendocs.sh for compatibility with Xcode 7.3. Reset toolchain to Swift 2.2 stable.

--- Revolver/gendocs.sh
+++ Revolver/gendocs.sh
@@ -5,10 +5,11 @@ TAG=1.0
 jazzy \
 --clean \
 --author "Petr Mánek" \
---author_url http://petrmanek.cz \
 --github_url https://github.com/petrmanek/Revolver \
+--author_url http://petrmanek.cz \
 --github-file-prefix https://github.com/petrmanek/Revolver/tree/$TAG \
 --module-version $TAG \
+--swift-version 2.2 \
 --xcodebuild-arguments -scheme,Revolver \
 --module Revolver \
 --output Documentation
