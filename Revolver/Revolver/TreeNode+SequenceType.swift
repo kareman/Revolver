1f8b2c7a03aeb8bde98887e3f4ff1e21f3bb3eca
Petr Mánek <petr.manek@seznam.cz>
2016-04-25 11:07:42 +0200

    Disabled AnyGenerator dependency.

--- Revolver/Revolver/TreeNode+SequenceType.swift
+++ Revolver/Revolver/TreeNode+SequenceType.swift
@@ -1,43 +1,43 @@
 
-extension TreeNode: SequenceType {
-    
-    /**
-     Enumerate subtree of this node by depth-first search.
-     
-     - returns: Generator of sequence of nodes in DFS order.
-     */
-    public func generate() -> AnyGenerator<TreeNode> {
-        var generatorStack = [IndexingGenerator<Array<TreeNode>>]()
-        generatorStack.append(treeNodeDescendants.generate())
-        
-        return AnyGenerator(body: {
-            while var generator = generatorStack.last {
-                let next = generator.next()
-                
-                if let nextNode = next {
-                    // Explore descendants of the generated node.
-                    generatorStack.append(nextNode.treeNodeDescendants.generate())
-                    return nextNode
-                } else {
-                    // The current node is out of descendants. Back away one level.
-                    generatorStack.popLast()
-                }
-            }
-            
-            // We have enumerated all nodes.
-            return nil
-        })
-    }
-    
-    /// Number if nodes in the subtree of this node (excluding this node).
-    public var totalNumberOfDescendants: Int {
-        var count = 0
-        
-        for descendant in treeNodeDescendants {
-            count += 1 + descendant.totalNumberOfDescendants
-        }
-        
-        return count
-    }
-    
-}
+//extension TreeNode: SequenceType {
+//    
+//    /**
+//     Enumerate subtree of this node by depth-first search.
+//     
+//     - returns: Generator of sequence of nodes in DFS order.
+//     */
+//    public func generate() -> AnyGenerator<TreeNode> {
+//        var generatorStack = [IndexingGenerator<Array<TreeNode>>]()
+//        generatorStack.append(treeNodeDescendants.generate())
+//        
+//        return AnyGenerator(body: {
+//            while var generator = generatorStack.last {
+//                let next = generator.next()
+//                
+//                if let nextNode = next {
+//                    // Explore descendants of the generated node.
+//                    generatorStack.append(nextNode.treeNodeDescendants.generate())
+//                    return nextNode
+//                } else {
+//                    // The current node is out of descendants. Back away one level.
+//                    generatorStack.popLast()
+//                }
+//            }
+//            
+//            // We have enumerated all nodes.
+//            return nil
+//        })
+//    }
+//    
+//    /// Number if nodes in the subtree of this node (excluding this node).
+//    public var totalNumberOfDescendants: Int {
+//        var count = 0
+//        
+//        for descendant in treeNodeDescendants {
+//            count += 1 + descendant.totalNumberOfDescendants
+//        }
+//        
+//        return count
+//    }
+//    
+//}
